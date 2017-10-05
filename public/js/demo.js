19b18d5992707d14ed2b22643606e7fbaabe4226
foozie3moons <matthew.bell.290@gmail.com>
2017-10-05 16:02:43 -0700

    switching to a new chart

--- public/js/demo.js
+++ public/js/demo.js
@@ -29,23 +29,21 @@ var colorLegendG = g.append("g")
   .attr("class", "color-legend")
   .attr("transform", "translate(235, 0)");
 
-var xScale = d3.scale.ordinal().rangeBands([0, innerWidth], barPadding);
-var yScale = d3.scale.linear().range([innerHeight, 0]);
-var colorScale = d3.scale.category10();
+var x = d3.scaleBand()
+  .range([0, innerWidth], barPadding);
+var y = d3.scaleLinear()
+  .range([innerHeight, 0]);
 
-var xAxis = d3.svg.axis().scale(xScale).orient("bottom")
-  .outerTickSize(0);
-var yAxis = d3.svg.axis().scale(yScale).orient("left")
-  .ticks(5)
-  .tickFormat(d3.format("s"))
-  .outerTickSize(0);
+var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-var colorLegend = d3.legend.color()
-  .scale(colorScale)
-  .shapePadding(2)
-  .shapeWidth(15)
-  .shapeHeight(15)
-  .labelOffset(4);
+xAxis = d3.select(".axis")
+    .call(d3.axisBottom(x));
+yAxis = d3.select('.axis')
+    .call(d3.axisLeft(y));
+
+var colorLegend = d3.legendColor()
+    .labelFormat(d3.format(".2f"))
+    .useClass(true);
 
 function renderGrouped(data){
 
@@ -53,31 +51,32 @@ function renderGrouped(data){
     .key(function (d){ return d[layerColumn]; })
     .entries(data)
 
-  var stack = d3.layout.stack()
-    .y(function (d){ return d[yColumn]; })
-    .values(function (d){ return d.values; });
+  var stack = d3.stack()
+    .keys(['paymentType', 'amount'])
+    .order(d3.stackOrderNone)
+    .offset(d3.stackOffsetNone);
 
   var layers = stack(nested);
 
-  xScale.domain(layers[0].values.map(function (d){
+  x.domain(layers.map(function (d){
     return d[xColumn];
   }));
 
-  yScale.domain([
-    0,
-    d3.max(layers, function (layer){
-      return d3.max(layer.values, function (d){
-        return d.y;
-      });
-    })
-  ]);
+  // y.domain([
+  //   0,
+  //   d3.max(layers, function (layer){
+  //     return d3.max(layer.values, function (d){
+  //       return d.y;
+  //     });
+  //   })
+  // ]);
 
-  colorScale.domain(layers.map(function (layer){
+  color.domain(layers.map(function (layer){
     return layer.key;
   }));
 
-  xAxisG.call(xAxis);
-  yAxisG.call(yAxis);
+  // xAxisG.call(xAxis);
+  // yAxisG.call(yAxis);
 
   var layers = g.selectAll(".layer").data(layers);
   layers.enter().append("g").attr("class", "layer");
@@ -89,16 +88,16 @@ function renderGrouped(data){
   var bars = layers.selectAll("rect").data(function (d){
     return d.values;
   });
-  var barWidth = xScale.rangeBand() / colorScale.domain().length;
+  var barWidth = x.bandWidth() / colorScale.domain().length;
   bars.enter().append("rect")
   bars.exit().remove();
   bars
     .attr("x", function (d, i, j){
-      return xScale(d[xColumn]) + barWidth * j;
+      return x(d[xColumn]) + barWidth * j;
     })
-    .attr("y", function (d){ return yScale(d.y); })
+    .attr("y", function (d){ return y(d.y); })
     .attr("width", barWidth)
-    .attr("height", function (d){ return innerHeight - yScale(d.y); })
+    .attr("height", function (d){ return innerHeight - y(d.y); })
 
   colorLegendG.call(colorLegend);
 }
@@ -115,11 +114,11 @@ function renderStacked(data) {
 
   var layers = stack(nested);
 
-  xScale.domain(layers[0].values.map(function (d){
+  x.domain(layers[0].values.map(function (d){
     return d[xColumn];
   }));
 
-  yScale.domain([
+  y.domain([
     0,
     d3.max(layers, function (layer){
       return d3.max(layer.values, function (d){
@@ -148,10 +147,10 @@ function renderStacked(data) {
   bars.enter().append("rect")
   bars.exit().remove();
   bars
-    .attr("x", function (d){ return xScale(d[xColumn]); })
-    .attr("y", function (d){ return yScale(d.y0 + d.y); })
-    .attr("width", xScale.rangeBand())
-    .attr("height", function (d){ return innerHeight - yScale(d.y); })
+    .attr("x", function (d){ return x(d[xColumn]); })
+    .attr("y", function (d){ return y(d.y0 + d.y); })
+    .attr("width", x.rangeBand())
+    .attr("height", function (d){ return innerHeight - y(d.y); })
 }
 
 var data = [];
